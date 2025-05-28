968daf53e8e71a86e19a7345a5af17e06d356f99
Oscar <kajns4.ad.reh.werfgd@gmail.com>
2025-05-28 00:59:02 -0500

    Actualización del código

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
